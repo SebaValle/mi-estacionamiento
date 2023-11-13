2ef543dc410bc25b71614de48390dcb63e0dde9a
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-12 23:54:13 -0300

    base de pagina

--- estacionamiento/anuncio/views.py
+++ estacionamiento/anuncio/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse("<h1>bienvenido a mi estacionamiento</h1>")
+    return render(request, 'paginas/inicio.html')
