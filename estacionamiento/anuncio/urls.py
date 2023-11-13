1d8a04d04f7da575b01fed9a7daa986a118ef22d
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-13 12:51:52 -0300

    arreglo de la urls

--- estacionamiento/anuncio/urls.py
+++ estacionamiento/anuncio/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('inicio', views.inicio, name='inicio'),
+    path('', views.inicio2, name='inicio2'),
+    path('anuncio', views.anuncio, name='anuncio'),
 ]
--- estacionamiento/anuncio/views.py
+++ estacionamiento/anuncio/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
+    return HttpResponse("<h1>hola a todos</h1>")
+def inicio2(request):
     return render(request, 'paginas/inicio.html')
+def anuncio(request):
+    return render(request, 'anuncio/index.html')
