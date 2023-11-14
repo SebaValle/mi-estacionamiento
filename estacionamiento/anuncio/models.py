8861d0b6c45c20ad9961d9c968cb6505abf4f7bb
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-13 21:06:31 -0300

    primer modelo de prueba y migracion

--- estacionamiento/anuncio/models.py
+++ estacionamiento/anuncio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class publicacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    dueno = models.CharField(max_length=100, verbose_name='dueño')
+    horario = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=200, verbose_name='descripcion')
+    precio = models.CharField(max_length=100, verbose_name='precio')
