baaefa55a2e5b3d066ef40755dbcdd53720cfe8d
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-13 22:02:02 -0300

    mostrar los datos, CRUD basico listo

--- estacionamiento/anuncio/views.py
+++ estacionamiento/anuncio/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import publicacion
 
 # Create your views here.
 def inicio(request):
     return HttpResponse("<h1>hola a todos</h1>")
+
 def inicio2(request):
     return render(request, 'paginas/inicio.html')
+
 def anuncio(request):
-    return render(request, 'anuncio/index.html')
+    publicaciones = publicacion.objects.all()
+    return render(request, 'anuncio/index.html', {'publicaciones': publicaciones})
+
 def crear(request):
     return render(request, 'anuncio/crear.html')
+
 def modificar(request):
     return render(request, 'anuncio/modificar.html')
