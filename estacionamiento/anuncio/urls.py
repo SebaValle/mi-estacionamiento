3c29c6020690489eb0ba81b899ce1c8ab1fbd964
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-22 22:14:37 -0300

    modificaciones de la presentacion informacion

--- estacionamiento/anuncio/urls.py
+++ estacionamiento/anuncio/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('inicio', views.inicio, name='inicio'),
-    path('', views.inicio2, name='inicio2'),
     path('anuncio', views.anuncio, name='anuncio'),
     path('anuncio/crear', views.crear, name='crear'),
     path('anuncio/modificar', views.modificar, name='modificar'),
-    path('anuncio/buscador', views.buscador, name='buscador'),
+    path('', views.buscador, name='buscador'),
 ]
