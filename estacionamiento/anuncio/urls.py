77e9e4bb8097c5432dac2c16a604fd7db0ca5679
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-22 23:23:23 -0300

    la configuracion de la imagenes funciona

--- estacionamiento/anuncio/urls.py
+++ estacionamiento/anuncio/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('inicio', views.inicio, name='inicio'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('anuncio/crear', views.crear, name='crear'),
     path('anuncio/modificar', views.modificar, name='modificar'),
     path('', views.buscador, name='buscador'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
