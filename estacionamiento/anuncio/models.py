44d6382ab2fb2d608f6a5ca2e0088a17e3c136da
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-22 21:17:48 -0300

    actualisasion de la base de datos

--- estacionamiento/anuncio/models.py
+++ estacionamiento/anuncio/models.py
@@ -5,9 +5,11 @@ from django.db import models
 class publicacion(models.Model):
     id = models.AutoField(primary_key=True)
     dueno = models.CharField(max_length=100, verbose_name='dueño')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name='imagen', null=True)
     horario = models.CharField(max_length=100)
-    descripcion = models.TextField(max_length=200, verbose_name='descripcion')
+    descripcion = models.TextField(max_length=300, verbose_name='descripcion')
     precio = models.CharField(max_length=100, verbose_name='precio')
+    estado = models.CharField(max_length=100, verbose_name='estado', default='libre')
 
     def __str__(self):
         fila = "dueño: " + self.dueno + " - " +  "descripcion: " + self.descripcion
